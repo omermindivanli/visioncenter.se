6cd938855895607fe0f713abfe042e8728b57981
Ömer Mindivanli <omer.mindivanli@gmail.com>
2018-01-27 23:26:09 +0300

    Resizing Jquery scripts were added to Navbar

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './Router/Router';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router />, document.getElementById('main'));
-
-registerServiceWorker();
